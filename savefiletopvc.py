dd1bce0a8c7d938d4ead3b5c608f4da1ec228471
jiabo.zhou <jiabo.zhou>
2021-06-04 15:11:51 +0000

    test

--- savefiletopvc.py
+++ savefiletopvc.py
@@ -9,16 +9,16 @@ from pathlib import Path
 def main(args):
     parser = argparse.ArgumentParser(description='Write prediction data to PVC')
     parser.add_argument("--srcdirname", type=str, required=True)
-    parser.add_argument("--subdirname1", type=str, required=True)
+    #parser.add_argument("--subdirname1", type=str, required=True)
     
     args = parser.parse_args(args)
     
     srcdirname = args.srcdirname
-    subdirname1 = args.subdirname1
+    #subdirname1 = args.subdirname1
     
-    filepath='/data/data/'+subdirname
-    Path(filepath).parent.mkdir(parents=True, exist_ok=True)
-    subprocess.run("cp -R "+ srcdirname + " /" , shell=True, capture_output=False)
+    filepath='/data/data/'
+    Path(filepath).mkdir(parents=True, exist_ok=True)
+    subprocess.run("cp -R "+ srcdirname + " "+filepath  , shell=True, capture_output=False)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
