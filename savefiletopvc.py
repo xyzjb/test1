c51a65b5d52e4a5c6f6cbee2e554cef5e32957e7
jiabo.zhou <jiabo.zhou>
2021-06-04 14:57:43 +0000

    test

---
+++ savefiletopvc.py
@@ -0,0 +1,26 @@
+import argparse
+import sys
+
+import subprocess
+
+from pathlib import Path
+
+    
+def main(args):
+    parser = argparse.ArgumentParser(description='Write prediction data to PVC')
+    parser.add_argument("--srcdirname", type=str, required=True)
+    parser.add_argument("--subdirname1", type=str, required=True)
+    
+    args = parser.parse_args(args)
+    
+    srcdirname = args.srcdirname
+    subdirname1 = args.subdirname1
+    
+    filepath='/data/data/'+subdirname
+    Path(filepath).parent.mkdir(parents=True, exist_ok=True)
+    subprocess.run("cp -R "+ srcdirname + " /" , shell=True, capture_output=False)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    
+    
